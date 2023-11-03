e079d2fcfdfb152e63b7f9fa3dadd0148ae9e73e
Mikus <mikus.plucejs@grobinasskola.lv>
2023-11-03 10:48:55 +0200

    commit

--- 2uzd.py
+++ 2uzd.py
@@ -2,3 +2,13 @@
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
 
 """
+import feedparser
+rss_url='https://news.google.com/home?hl=lv&gl=LV&ceid=LV:lv'
+
+lll=feedparser.parse(rss_url)
+
+for entry in lll.entries:
+    print('Virsraksts:', entry.title)
+    print('Saite:', entry.link)
+    print('Datums:', entry.published)
+    print('\n')
